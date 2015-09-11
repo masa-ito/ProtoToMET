84996ddc3ce53f77c0f1516e51b99018122bdb0c
Masakatsu ITO <itoh.masakatsu@jaxa.jp>
2015-09-11 11:17:05 +0900

    proto::display_expr( proto::as_expr<VecDomain>( ( v2 + v3 )[ 2 ] ) ) of transformingVectorOnHeapArray.cpp resulted in infinite looping

--- transformingVectorOnHeapArray.cpp
+++ transformingVectorOnHeapArray.cpp
@@ -51,7 +51,7 @@ struct VecExpr
 
 		const double& operator [](int i) const {
 			proto::_default<> trans;
-			std::cout << "VecExpr const double& operator [](int i) const used." << std::endl;
+			std::cout << "const double& VecExpr<Expr>::operator [](int i) const used." << std::endl;
 			return trans( VecExprOpt()(  proto::as_expr<VecDomain>( (*this)[i] )  ) );
 			// return (*this)[i];
 		}
@@ -129,9 +129,11 @@ int main()
     Vector v1( 4, 1.0 ), v2( 4, 2.0 ), v3( 4, 3.0 );
 
     // Add two vectors lazily and get the 2nd element.
-    double d1 = ( v2 + v3 )[ 2 ];   // Look ma, no temporaries!
+    // Checking if code optimization works properly.
+    proto::display_expr( proto::as_expr<VecDomain>( ( v2 + v3 )[ 2 ] ) );
+    // double d1 = ( v2 + v3 )[ 2 ];   // Look ma, no temporaries!
     //double d1 = v2[2] + v3[2];   // Look ma, no temporaries!
-    std::cout << d1 << std::endl;
+    // std::cout << d1 << std::endl;
 
     // Subtract two vectors and add the result to a third vector.
     /* v1 += v2 - v3;                  // Still no temporaries!
