93b4b01f3324b12e8295500b6e695a0a2345ca9d
Masakatsu ITO <itoh.masakatsu@jaxa.jp>
2016-05-16 16:57:10 +0900

    matVecMultAndVecSubMetaOpenMP.cpp worked correctly.

--- src/matVecMultAndVecSubMetaOpenMP.hpp
+++ src/matVecMultAndVecSubMetaOpenMP.hpp
@@ -27,60 +27,167 @@ namespace DenseLinAlg {
 	// Callable transform object to make a proto exression
 	// for lazily evaluating multiplication
 	struct MatVecMult;
+	struct MatVecMultOmp;
 
 	// The grammar for the multiplication of a matrix and a vector
 	struct MatVecMultGrammar : proto::or_<
 		proto::when<
-			proto::multiplies< proto::terminal< Matrix> ,
+			proto::multiplies< proto::terminal< Matrix > ,
 								proto::terminal< Vector> >,
 			MatVecMult( proto::_value( proto::_left),
 						proto::_value( proto::_right) )
 		>
 	> {};
 
+	// The grammar for the multiplication of a matrix and a vector
+	struct MatVecMultOmpGrammar : proto::or_<
+		proto::when<
+			proto::multiplies< proto::terminal< Matrix > ,
+								proto::terminal< Vector > >,
+			MatVecMultOmp( proto::_value( proto::_left),
+						proto::_value( proto::_right) )
+		>
+	> {};
+
+
 	// The transformation rule for vector element expressions
 	// This transform accepts a subscript index  of an expression being parsed
 	// as the state variable,and distribute that index over the child nodes.
-	struct VecElmGrammar : proto::or_<
+	struct VecElementwiseElmGrammar : proto::or_<
 		// Vector
-		proto::when< proto::terminal< Vector>,
+		proto::when< proto::terminal< Vector >,
 					proto::_make_function( proto::_, proto::_state) >,
-		// Matrix * Vector
-		proto::when< MatVecMultGrammar,
-					proto::_make_function( MatVecMultGrammar( proto::_),
-											proto::_state) >,
+
 		// double * Vector
 
 		// DiagonalMatrix * Vector
 
-		// VecElmGrammar +(-) VecElmGrammar
-		proto::plus< VecElmGrammar, VecElmGrammar> ,
-		proto::minus< VecElmGrammar, VecElmGrammar>
+		// VecElementwiseElmGrammar +(-) VecElementwiseElmGrammar
+		proto::plus< VecElementwiseElmGrammar, VecElementwiseElmGrammar > ,
+		proto::minus< VecElementwiseElmGrammar, VecElementwiseElmGrammar >
 	> {};
 
+	// The transformation rule for vector element expressions
+	// This transform accepts a subscript index  of an expression being parsed
+	// as the state variable,and distribute that index over the child nodes.
+	struct VecReductionElmGrammar : proto::or_<
+		// Matrix * Vector
+		proto::when< MatVecMultGrammar,
+					proto::_make_function( MatVecMultGrammar( proto::_),
+											proto::_state) >,
+
+		// VecReductionElmGrammar +(-) VecElementwiseElmGrammar
+		proto::plus< VecReductionElmGrammar, VecElementwiseElmGrammar > ,
+		proto::minus< VecReductionElmGrammar, VecElementwiseElmGrammar > ,
+
+		// VecElementwiseElmGrammar +(-) VecReductionElmGrammar
+		proto::plus< VecElementwiseElmGrammar, VecReductionElmGrammar > ,
+		proto::minus< VecElementwiseElmGrammar, VecReductionElmGrammar >
+	> {};
+
+	// The transformation rule for vector element expressions
+	// This transform accepts a subscript index  of an expression being parsed
+	// as the state variable,and distribute that index over the child nodes.
+	struct VecReductionElmOmpGrammar : proto::or_<
+		// Matrix * Vector
+		proto::when< MatVecMultOmpGrammar,
+					proto::_make_function( MatVecMultOmpGrammar( proto::_),
+											proto::_state) >,
+
+		// VecReductionElmOmpGrammar +(-) VecReductionElmOmpGrammar
+		proto::plus< VecReductionElmOmpGrammar,
+					VecReductionElmOmpGrammar > ,
+		proto::minus< VecReductionElmOmpGrammar,
+					VecReductionElmOmpGrammar >,
+
+		// VecReductionElmOmpGrammar +(-) VecElementwiseElmGrammar
+		proto::plus< VecReductionElmOmpGrammar, VecElementwiseElmGrammar > ,
+		proto::minus< VecReductionElmOmpGrammar, VecElementwiseElmGrammar > ,
+
+		// VecElementwiseElmGrammar +(-) VecReductionElmOmpGrammar
+		proto::plus< VecElementwiseElmGrammar, VecReductionElmOmpGrammar > ,
+		proto::minus< VecElementwiseElmGrammar, VecReductionElmOmpGrammar >
+	> {};
+
+
 	// The grammar for a vector expression
-	struct VecExprGrammar : proto::or_<
-		// VecElmGrammar( index )
+	struct VecElementwiseGrammar : proto::or_<
+		// VecElementwiseElmGrammar( index )
 		proto::when<
-			proto::function< VecElmGrammar, proto::_ >,
-			proto::_default< >( VecElmGrammar(proto::_left, proto::_right) )
+			proto::function< VecElementwiseElmGrammar, proto::_ >,
+			proto::_default< >(
+				VecElementwiseElmGrammar(proto::_left, proto::_right) )
 		>,
 
 		// Vector
 		proto::terminal< Vector >,
 
-		// VecExprGrammar +(-) VecExprGrammar
-		proto::plus< VecExprGrammar, VecExprGrammar> ,
-		proto::minus< VecExprGrammar, VecExprGrammar>,
+		// VecElementwiseGrammar +(-) VecElementwiseGrammar
+		proto::plus< VecElementwiseGrammar, VecElementwiseGrammar > ,
+		proto::minus< VecElementwiseGrammar, VecElementwiseGrammar > //,
 
 		// Vector * double , or double * Vector
-
-		// Matrix * Vector
-		MatVecMultGrammar //,
 
 		// DiagonalMatrix * VecExprGrammar
 	> {};
 
+	// The grammar for a vector expression
+	struct VecReductionGrammar : proto::or_<
+		// VecReductionElmGrammar( index )
+		proto::when<
+			proto::function< VecReductionElmGrammar, proto::_ >,
+			proto::_default< >(
+				VecReductionElmGrammar(proto::_left, proto::_right) )
+		>,
+
+		// VecReductionGrammar +(-) VecReductionGrammar
+		proto::plus< VecReductionGrammar, VecReductionGrammar > ,
+		proto::minus< VecReductionGrammar, VecReductionGrammar >,
+
+		// VecReductionGrammar +(-) VecElementwiseGrammar
+		proto::plus< VecReductionGrammar, VecElementwiseGrammar > ,
+		proto::minus< VecReductionGrammar, VecElementwiseGrammar >,
+
+		// VecElementwiseGrammar +(-) VecReductionGrammar
+		proto::plus< VecElementwiseGrammar, VecReductionGrammar > ,
+		proto::minus< VecElementwiseGrammar, VecReductionGrammar >,
+
+		// Matrix * Vector
+		MatVecMultGrammar //,
+	> {};
+
+	// The grammar for a vector expression
+	struct VecReductionOmpGrammar : proto::or_<
+		// VecReductionElmOmpGrammar( index )
+		proto::when<
+			proto::function< VecReductionElmOmpGrammar, proto::_ >,
+			proto::_default< >(
+				VecReductionElmOmpGrammar(proto::_left, proto::_right) )
+		>,
+
+		// VecReductionOmpGrammar +(-) VecReductionOmpGrammar
+		proto::plus< VecReductionOmpGrammar, VecReductionOmpGrammar > ,
+		proto::minus< VecReductionOmpGrammar, VecReductionOmpGrammar >,
+
+		// VecReductionOmpGrammar +(-) VecElementwiseGrammar
+		proto::plus< VecReductionOmpGrammar, VecElementwiseGrammar > ,
+		proto::minus< VecReductionOmpGrammar, VecElementwiseGrammar >,
+
+		// VecElementwiseGrammar +(-) VecReductionOmpGrammar
+		proto::plus< VecElementwiseGrammar, VecReductionOmpGrammar > ,
+		proto::minus< VecElementwiseGrammar, VecReductionOmpGrammar >,
+
+		// Matrix * Vector
+		MatVecMultOmpGrammar //,
+	> {};
+
+
+	// The grammar for a vector expression
+	struct VecExprGrammar : proto::or_<
+		VecElementwiseGrammar,
+		VecReductionGrammar,
+		VecReductionOmpGrammar
+	> {};
 
 	// The transformation rule for matrix element expressions
 	struct MatElmGrammar : proto::or_<
@@ -184,11 +291,11 @@ namespace DenseLinAlg {
 			// std::cout << "Created" << std::endl;
 		}
 
-		Vector(const Vector& vec) :
+		/* Vector(const Vector& vec) :
 			sz( vec.sz), data( new double[sz] ) {
 			for (int i = 0; i < sz; i++) data[i] = vec.data[i];
 			// std::cout << "Copied! " << std::endl;
-		}
+		} */
 
 		/* template < typename Derived >
 		Vector( const LazyVectorMaker< Derived > & maker) :
@@ -206,30 +313,59 @@ namespace DenseLinAlg {
 		int rowSize() const { return 1; }
 		int columnSize() const { return sz; }
 
-		double dot( const Vector& vec) const
+		/* double dot( const Vector& vec) const
 		{
 			double d = data[0] * vec.data[0];
 			for (int i = 1; i < sz; i++) d += data[i] * vec.data[i];
 			return d;
-		}
+		} */
 
-		double abs() const {
+		/* double abs() const {
 			double aSqr = data[0] * data[0];
 			for (int i = 1; i < sz; i++) aSqr += data[i] * data[i];
 			return sqrt( aSqr);
-		}
+		} */
 
 		// accessing to an element of this vector
 		double& operator()(int i) { return data[i]; }
 		const double& operator()(int i) const { return data[i]; }
 
-		// assigning the lhs of a vector expression into this vector
 		template < typename Expr >
-		Vector& operator=( const ExprWrapper< Expr >& expr ) {
-			// proto::_default<> trans;
+		void assign( const Expr& expr ,
+			const PTT::SingleProcess< PTT::SingleThread< PTT::NoSIMD > >& ) {
 			for(int i=0; i < sz; ++i)
 				data[i] = VecExprGrammar()( expr(i) );
-				// data[i] = trans( VecExprGrammar()( expr(i) ) );
+		}
+
+		template < typename Expr >
+		typename boost::enable_if<
+			proto::matches< Expr, VecElementwiseGrammar >
+		>::type
+		assign( const Expr& expr ,
+				const PTT::SingleProcess< PTT::OpenMP< PTT::NoSIMD > >& ) {
+			#pragma omp parallel for
+			for(int i=0; i < sz; ++i)
+				data[i] = VecElementwiseGrammar()( expr(i) );
+
+			std::cout << "OpenMP elementwise assign" << std::endl;
+		}
+
+		template < typename Expr >
+		typename boost::enable_if<
+			proto::matches< Expr,VecReductionGrammar >
+		>::type
+		assign( const Expr& expr ,
+				const PTT::SingleProcess< PTT::OpenMP< PTT::NoSIMD > >& ) {
+			for(int i=0; i < sz; ++i)
+				data[i] = VecReductionOmpGrammar()( expr(i) );
+
+			std::cout << "OpenMP reduction assign" << std::endl;
+		}
+
+		// assigning the lhs of a vector expression into this vector
+		template<typename Expr>
+		Vector& operator=( const Expr& expr ) {
+			assign( expr, PTT::Specified() );
 			return *this;
 		}
 
@@ -241,25 +377,21 @@ namespace DenseLinAlg {
 
 
 		// assigning and adding the lhs of a vector expression into this vector
-		template<typename Expr>
+		/* template<typename Expr>
 		Vector& operator+=( const Expr& expr ) {
-			// proto::_default<> trans;
 			for(int i=0; i < sz; ++i)
 				data[i] += VecExprGrammar()( expr(i) );
-				// data[i] += trans( VecExprGrammar()( expr(i) ) );
 			return *this;
-		}
+		} */
 
 		// assigning and subtracting the lhs of a vector expression into
 		// this vector
-		template<typename Expr>
+		/* template<typename Expr>
 		Vector& operator-=( const Expr& expr ) {
-			// proto::_default<> trans;
 			for(int i=0; i < sz; ++i)
 				data[i] -= VecExprGrammar()( expr(i) );
-				// data[i] -= trans( VecExprGrammar()( expr(i) ) );
 			return *this;
-		}
+		} */
 
 		/* friend void diagPrecondConGrad( Vector & ansVec,
 				const Matrix & coeffMat, const Vector & rhsVec,
@@ -292,7 +424,7 @@ namespace DenseLinAlg {
 				for (int ci = 0; ci < colSz; ci++) m[ri][ci] = iniVal;
 		}
 
-		Matrix( const Matrix& mat) :
+		/* Matrix( const Matrix& mat) :
 			rowSz( mat.rowSz), colSz( mat.colSz),
 			data( new double[rowSz*colSz] ), m( new double*[rowSz])
 		{
@@ -300,7 +432,7 @@ namespace DenseLinAlg {
 				for (int ri = 0; ri < rowSz; ri++)
 					for (int ci = 0; ci < colSz; ci++)
 						m[ri][ci] = mat.m[ri][ci];
-		}
+		} */
 
 		~Matrix()
 		{
@@ -316,13 +448,13 @@ namespace DenseLinAlg {
 		const double& operator()(int ri, int ci) const { return m[ri][ci]; }
 
 		// assigning the lhs of a vector expression into this matrix
-		template<typename Expr>
+		/* template<typename Expr>
 		Matrix& operator=( const ExprWrapper< Expr >& expr ) {
 			for(int ri=0; ri < rowSz; ri++)
 				for (int ci=0; ci < colSz; ci++ )
 					m[ri][ci] = MatExprGrammar()( expr(ri, ci) );
 			return *this;
-		}
+		} */
 
 		/* template < typename Derived >
 		Matrix& operator=( const LazyMatrixMaker< Derived >& maker) {
@@ -331,23 +463,23 @@ namespace DenseLinAlg {
 		} */
 
 		// assigning and adding the lhs of a vector expression into this matrix
-		template<typename Expr>
+		/* template<typename Expr>
 		Matrix& operator+=( const Expr& expr ) {
 			for(int ri=0; ri < rowSz; ri++)
 				for (int ci=0; ci < colSz; ci++ )
 					m[ri][ci] += MatExprGrammar()( expr(ri, ci) );
 			return *this;
-		}
+		}*/
 
 		// assigning and subtracting the lhs of a vector expression into
 		// this matrix
-		template<typename Expr>
+		/* template<typename Expr>
 		Matrix& operator-=( const Expr& expr ) {
 			for(int ri=0; ri < rowSz; ri++)
 				for (int ci=0; ci < colSz; ci++ )
 					m[ri][ci] -= MatExprGrammar()( expr(ri, ci) );
 			return *this;
-		}
+		} */
 
 		/* friend void diagPrecondConGrad( Vector & ansVec,
 				const Matrix & coeffMat, const Vector & rhsVec,
@@ -370,7 +502,6 @@ namespace DenseLinAlg {
 		const int mColSz;
 
 		typedef double result_type;
-		// typedef mpl::int_<1> proto_arity;
 
 		explicit LazyMatVecMult(Matrix const& mat, Vector const& vec) :
 			m( mat), v( vec), mColSz(mat.rowSize()) {}
@@ -381,6 +512,31 @@ namespace DenseLinAlg {
 		result_type operator()(int index) const
 		{
 			result_type elm = 0.0;
+			for (int ci =0;  ci < mColSz; ci++)
+				elm += m(index, ci) * v(ci);
+			return elm;
+		}
+	};
+
+	struct LazyMatVecMultOmp
+	{
+		Matrix const& m;
+		Vector const& v;
+		const int mColSz;
+
+		typedef double result_type;
+
+		explicit LazyMatVecMultOmp(Matrix const& mat, Vector const& vec) :
+			m( mat), v( vec), mColSz(mat.rowSize()) {}
+
+		LazyMatVecMultOmp( LazyMatVecMultOmp const& lazy) :
+			m(lazy.m), v(lazy.v), mColSz(lazy.mColSz) {}
+
+		result_type operator()(int index) const
+		{
+			result_type elm = 0.0;
+
+			#pragma omp parallel for reduction (+:elm)
 			for (int ci =0;  ci < mColSz; ci++)
 				elm += m(index, ci) * v(ci);
 			return elm;
@@ -401,6 +557,19 @@ namespace DenseLinAlg {
 		}
 	};
 
+	// Callable transform object to make the lazy functor
+	// a proto exression for lazily evaluationg the multiplication
+	// of a matrix and a vector .
+	struct MatVecMultOmp : proto::callable
+	{
+		typedef proto::terminal< LazyMatVecMultOmp >::type result_type;
+
+		result_type
+		operator()( Matrix const& mat, Vector const& vec) const
+		{
+			return proto::as_expr( LazyMatVecMultOmp(mat, vec) );
+		}
+	};
 
 	// Define a trait for detecting linear algebraic terminals, to be used
 	// by the BOOST_PROTO_DEFINE_OPERATORS macro below.
@@ -421,7 +590,7 @@ namespace DenseLinAlg {
 }
 
 
-namespace DLA = DenseLinAlg;
+// namespace DLA = DenseLinAlg;
 
 void testMatVecMultAndVecAdd()
 {
@@ -442,17 +611,44 @@ void testMatVecMultAndVecAdd()
     vecB(1) = 5.0;
     vecB(2) = 6.0;
 
-	GrammarChecker< ExprGrammar >
-		checker = GrammarChecker< ExprGrammar >();
+	//GrammarChecker< ExprGrammar >
+	//	checker = GrammarChecker< ExprGrammar >();
 
-    std::cout << "Checking if (vecB - matA * vecX) matches to ExprGrammar ..."
-        	<< std::endl;
-	checker( vecB - matA * vecX );
+    // Vector object alons is not an expression template.
+	// checker( vecB );
+
+    std::cout
+		<< "Checking if (vecB - vecX) matches to VecElementwiseGrammar ..."
+        << std::endl;
+    GrammarChecker< VecElementwiseGrammar >()( vecB - vecX );
+
+    std::cout
+		<< "Checking if (vecB - vecX)(2) matches to VecElementwiseGrammar  ..."
+        << std::endl;
+    GrammarChecker< VecElementwiseGrammar >()( ( vecB - vecX )(2) );
+
+
+    std::cout
+		<< "Checking if (matA * vecX) matches to VecReductionGrammar ..."
+        << std::endl;
+    GrammarChecker< VecReductionGrammar >()( matA * vecX );
+
+    std::cout
+		<< "Checking if (matA * vecX)(2) matches to VecReductionGrammar..."
+        << std::endl;
+    GrammarChecker< VecReductionGrammar >()( ( matA * vecX )(2) );
+
+
+    std::cout
+		<< "Checking if (vecB - matA * vecX) "
+		<< "matches to VecReductionGrammar ..."
+        << std::endl;
+    GrammarChecker< VecReductionGrammar >()( vecB - matA * vecX );
 
     std::cout << "Checking if ( vecB - matA * vecX)(2) "
-    		<< "matches to ExprGrammar ..."
+    		<< "matches to VecReductionGrammar ..."
         	<< std::endl;
-	checker( ( vecB - matA * vecX)(2) );
+    GrammarChecker< VecReductionGrammar >()( ( vecB - matA * vecX)(2) );
 
     double elm2 = VecExprGrammar()( ( vecB - matA * vecX)(2) );
 
