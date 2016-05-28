f17b00e52330280df3f7386acc6eb0a2ad22dafb
Masakatsu ITO <itoh.masakatsu@jaxa.jp>
2016-05-28 23:37:39 +0900

    diagPrecondConjGrad_IntroToCFD_Exam4_3.cpp and diagPrecondConjGrad_IntroToCFD_Exam4_3_plainC.cpp were modified to take two command line aruguments, the num. of grid points and the num. of elapsed time measurements.

--- src/test/diagPrecondConjGrad_IntroToCFD_Exam4_3.cpp
+++ src/test/diagPrecondConjGrad_IntroToCFD_Exam4_3.cpp
@@ -21,84 +21,96 @@
 
 int main(int argc, char *argv[]) {
 
-	int NumCtrlVol = 5;
+	int NumCtrlVol = 5, NumMeasurement = 1;
 	if ( argc > 1 ) NumCtrlVol = atoi( argv[1] );
-
 	std::cout << "The num. of grid points = " << NumCtrlVol << std::endl;
+
+	if ( argc > 2 ) NumMeasurement = atoi( argv[2] );
+	std::cout << "The num. of measurment = " << NumMeasurement << std::endl;
 
 	printConstants();
 
-	DLA::Matrix coeffMat( NumCtrlVol, NumCtrlVol, 0.0);
+	double elapsedTimeSum = 0.0;
 
-	double deltaX = CylinderLength / NumCtrlVol,
-			deltaDirichlet = deltaX / 2.0;
+	for (int iM = 0; iM < NumMeasurement; iM++ ) {
 
-	const double scale = - ThermalConductivity * Area;
-	std::cout << "scale = - ThermalConductivity * Area" <<
-			std::endl;
-	std::cout << "= " << scale << std::endl;
+		DLA::Matrix coeffMat( NumCtrlVol, NumCtrlVol, 0.0);
 
-	const double nSqr =  ConvectiveHeatTransCoeff * Circumference /
-						( ThermalConductivity * Area );
+		double deltaX = CylinderLength / NumCtrlVol,
+				deltaDirichlet = deltaX / 2.0;
 
-	coeffMat(0,0) = ( 1.0 / deltaDirichlet // Dirichlet condition term
-			          + 1.0 / deltaX + nSqr * deltaX ) * scale;
+		const double scale = - ThermalConductivity * Area;
+		if ( NumMeasurement < 2 ) {
+			std::cout << "scale = - ThermalConductivity * Area" <<
+				std::endl;
+			std::cout << "= " << scale << std::endl;
+		}
 
-	coeffMat( NumCtrlVol-1, NumCtrlVol-1) =
-			( 2.0 / deltaX
-			  - 1.0 / deltaX // Neumann condition term
-			  + nSqr * deltaX ) * scale;
+		const double nSqr =  ConvectiveHeatTransCoeff * Circumference /
+							( ThermalConductivity * Area );
 
-	coeffMat(0, 1) = - 1.0 / deltaX * scale;
-	coeffMat( NumCtrlVol-1, NumCtrlVol-2) = - 1.0 / deltaX * scale;
+		coeffMat(0,0) = ( 1.0 / deltaDirichlet // Dirichlet condition term
+						  + 1.0 / deltaX + nSqr * deltaX ) * scale;
 
-	int i;
-	for (i = 1; i < NumCtrlVol-1; i++) {
-		coeffMat( i, i-1) = - 1.0 / deltaX * scale;
-		coeffMat( i, i) = ( 2.0 / deltaX + nSqr * deltaX) * scale;
-		coeffMat( i, i+1) = - 1.0 / deltaX * scale;
+		coeffMat( NumCtrlVol-1, NumCtrlVol-1) =
+				( 2.0 / deltaX
+				  - 1.0 / deltaX // Neumann condition term
+				  + nSqr * deltaX ) * scale;
+
+		coeffMat(0, 1) = - 1.0 / deltaX * scale;
+		coeffMat( NumCtrlVol-1, NumCtrlVol-2) = - 1.0 / deltaX * scale;
+
+		int i;
+		for (i = 1; i < NumCtrlVol-1; i++) {
+			coeffMat( i, i-1) = - 1.0 / deltaX * scale;
+			coeffMat( i, i) = ( 2.0 / deltaX + nSqr * deltaX) * scale;
+			coeffMat( i, i+1) = - 1.0 / deltaX * scale;
+		}
+
+		DLA::Vector rhsVec( NumCtrlVol);
+
+		rhsVec(0) = ( 2.0 / deltaX * HotTemperature // Dirichlet condition
+					  + nSqr * deltaX * AmbientTemperature ) * scale;
+		for (i = 1; i < NumCtrlVol; i++)
+			rhsVec( i) = nSqr * deltaX * AmbientTemperature * scale;
+
+		if ( NumMeasurement < 2 ) {
+			printCoefficients( coeffMat, scale);
+			printRHS( rhsVec, scale);
+		}
+
+		SLA::DiagonalPreconditioner precond( coeffMat);
+		SLA::ConjugateGradient< DLA::Matrix, SLA::DiagonalPreconditioner >
+													cg( coeffMat, precond);
+
+		const DLA::Vector tempGuess( NumCtrlVol, (100.0 + 20.0) / 2.0);
+		const double convergenceCriterion = 1.0e-7;
+		// const int maxIter = 100;
+
+		DLA::Vector temperature( NumCtrlVol);
+
+		// Measuring the elapsed time of our conjugate gradient procedure
+		auto start = std::chrono::system_clock::now();
+
+		temperature = cg.solve(rhsVec, tempGuess, convergenceCriterion);
+
+		auto end = std::chrono::system_clock::now();
+		auto diff = end - start;
+
+		elapsedTimeSum +=
+			double( std::chrono::duration_cast<std::chrono::milliseconds>
+														(diff).count() );
+
+		if ( NumMeasurement < 2 )
+			printCalculatedAndExactTemperatureDistributions< DLA::Vector >(
+															temperature);
 	}
 
-	DLA::Vector rhsVec( NumCtrlVol);
-
-	rhsVec(0) = ( 2.0 / deltaX * HotTemperature // Dirichlet condition
-				  + nSqr * deltaX * AmbientTemperature ) * scale;
-	for (i = 1; i < NumCtrlVol; i++)
-		rhsVec( i) = nSqr * deltaX * AmbientTemperature * scale;
-
-	if ( NumCtrlVol < 50 ) {
-		printCoefficients( coeffMat, scale);
-		printRHS( rhsVec, scale);
-	}
-
-	SLA::DiagonalPreconditioner precond( coeffMat);
-	SLA::ConjugateGradient< DLA::Matrix, SLA::DiagonalPreconditioner >
-	                   							cg( coeffMat, precond);
-
-	const DLA::Vector tempGuess( NumCtrlVol, (100.0 + 20.0) / 2.0);
-	const double convergenceCriterion = 1.0e-7;
-	// const int maxIter = 100;
-
-	DLA::Vector temperature( NumCtrlVol);
-
-	// Measuring the elapsed time of our conjugate gradient procedure
-	auto start = std::chrono::system_clock::now();
-
-	temperature = cg.solve(rhsVec, tempGuess, convergenceCriterion);
-
-	auto end = std::chrono::system_clock::now();
-
-	auto diff = end - start;
 	std::cout << std::endl;
 	std::cout << "elapsed time of conjugate gradient = "
-	  << std::chrono::duration_cast<std::chrono::milliseconds>(diff).count()
-		  << " msec."
-		  << std::endl;
-
-
-	if ( NumCtrlVol < 50 )
-		printCalculatedAndExactTemperatureDistributions< DLA::Vector >(
-															temperature);
+	  << elapsedTimeSum / NumMeasurement
+	  << " msec."
+	  << std::endl;
 
 	return 0;
 }
